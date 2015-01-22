fee5a984e23a0d48368fc2a78d20681ed352fcab
Gatermann <gatermann@effective-webwork.de>
2015-01-22 16:58:27 +0100

    depends static_info_tables

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,6 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2',
+                        'static_info_tables' => '6.1.2',                       
 		),
 		'conflicts' => array(
 		),
