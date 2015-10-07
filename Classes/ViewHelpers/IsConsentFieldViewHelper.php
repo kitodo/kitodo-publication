285b747c43454cb63d051ce8febb4c2d6aafa703
Gatermann <gatermann@effective-webwork.de>
2015-10-07 17:47:48 +0200

    Bug fixing: consent field.

---
+++ Classes/ViewHelpers/IsConsentFieldViewHelper.php
@@ -0,0 +1,20 @@
+<?php
+namespace EWW\Dpf\ViewHelpers;
+
+class IsConsentFieldViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper {
+    
+    /**
+     *
+     * @param boolean $condition
+     * @return string     
+     */
+    public function render($condition) {
+        if ((TYPO3_MODE === 'BE') && $condition ) {            
+            return $this->renderThenChild();
+        }
+        return $this->renderElseChild();
+    }
+   
+}
+
+?>
