b96e691271e78cf8514157f6fda5808949b895d3
hauke <gatermann@effective-webwork.de>
2015-11-27 18:54:49 +0100

    data types

--- ext_tables.sql
+++ ext_tables.sql
@@ -153,6 +153,7 @@ CREATE TABLE tx_dpf_domain_model_metadataobject (
 	display_name varchar(255) DEFAULT '' NOT NULL,
 	max_iteration int(11) DEFAULT '0' NOT NULL,
 	mandatory tinyint(1) unsigned DEFAULT '0' NOT NULL,
+        data_type varchar(255) DEFAULT '' NOT NULL,
         validation varchar(255) DEFAULT '' NOT NULL,
 	mapping varchar(255) DEFAULT '' NOT NULL,
         mods_extension tinyint(1) unsigned DEFAULT '0' NOT NULL,        
