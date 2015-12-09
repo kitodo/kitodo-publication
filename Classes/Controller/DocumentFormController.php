a48d9dbda0f87f20381c5c52a7671f31b6932a50
Gatermann <gatermann@effective-webwork.de>
2015-12-09 17:19:00 +0100

    allow download as default

--- Classes/Controller/DocumentFormController.php
+++ Classes/Controller/DocumentFormController.php
@@ -13,6 +13,26 @@ class DocumentFormController extends AbstractDocumentFormController {
     $this->redirect('list','DocumentForm',NULL,array('success' => $success));    
   }
   
+  
+  /**
+    * action create
+    *
+    * @param \EWW\Dpf\Domain\Model\DocumentForm $newDocumentForm
+    * @return void
+    */
+    public function createAction(\EWW\Dpf\Domain\Model\DocumentForm $newDocumentForm) {
+        
+        foreach ( $newDocumentForm->getNewFiles() as $newFile ) {                                      
+            if (empty($newFile->getUID())) {
+                $newFile->setDownload(TRUE);
+            }    
+            $files[] = $newFile;
+        }
+        
+        $newDocumentForm->setNewFiles($files);
+        
+        parent::createAction($newDocumentForm);
+    }  
 }
 
 ?>
