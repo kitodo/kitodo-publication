f340a11d81fa35e5e6a194f74d24d41fd17ab4dd
Alexander Bigga <alexander.bigga@slub-dresden.de>
2015-12-09 16:17:52 +0100

    run composer update to make search working

--- Lib/ElasticSearchPhpClient/vendor/composer/autoload_psr4.php
+++ Lib/ElasticSearchPhpClient/vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
