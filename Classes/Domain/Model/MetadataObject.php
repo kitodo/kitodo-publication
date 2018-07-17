13d14b52b597720d461c8f252bf31257af94ad93
haogatyp <gatermann@effective-webwork.de>
2018-07-17 18:54:30 +0200

    A new input field type with a dropdown box containing selectable value options. Alternatively, a value can also be entered directly.

--- Classes/Domain/Model/MetadataObject.php
+++ Classes/Domain/Model/MetadataObject.php
@@ -68,6 +68,7 @@ class MetadataObject extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     const select   = 2;
     const checkbox = 3;
     const hidden   = 4;
+    const INPUTDROPDOWN = 100;
 
     const INPUT_DATA_TYPE_REGEXP = "REGEXP";
     const INPUT_DATA_TYPE_DATE   = "DATE";
