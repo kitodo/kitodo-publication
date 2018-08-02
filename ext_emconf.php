28f3aaf95289ee3603b77abe5ed4aa5bb3e2765e
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2018-08-02 16:35:05 +0200

    Set TYPO3 dependency to 6.2.0-7.6.99
    Resolves #68

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version'          => '1.3.6',
     'constraints'      => array(
         'depends'   => array(
-            'typo3' => '6.2.0-6.2.99',
+            'typo3' => '6.2.0-7.6.99',
             'static_info_tables' => '6.3.0'
         ),
         'conflicts' => array(
