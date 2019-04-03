0916fc514fa0246228cedc1bb391ccec32d608db
Christopher Timm <timm@effective-webwork.de>
2019-04-03 09:33:31 +0200

    add Tests for XPathXMLGenerator

---
+++ Tests/Unit/Services/XPathXMLGeneratorTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace EWW\Dpf\Tests\Unit\Services;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+class XPathXMLGeneratorTest extends UnitTestCase
+{
+
+    /**
+     * @var XPathXMLGenerator
+     */
+    private $xpathGenerator = null;
+
+    protected function setUp() {
+        $this->xpathGenerator = new \EWW\Dpf\Services\XPathXMLGenerator();
+    }
+
+    /**
+     * @test
+     */
+    public function testXMLGeneration1()
+    {
+        $xpath = 'mods:name[mods:role/mods:roleTerm[@type="code"][@authority="marcrelator"]="edt"]/mods:displayForm';
+        $expectedXml = '<mods:name><mods:role><mods:roleTerm type="code" authority="marcrelator">edt</mods:roleTerm></mods:role><mods:displayForm/></mods:name>';
+
+        $this->xpathGenerator->loop($xpath);
+
+        $this->assertEquals(
+            $expectedXml,
+            $this->xpathGenerator->getXML()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function testXMLGeneration2()
+    {
+        $xpath = 'mods:language/mods:languageTerm[@authority="iso639-2b"][@type="code"]="ger"';
+        $expectedXml = '<mods:language><mods:languageTerm authority="iso639-2b" type="code">ger</mods:languageTerm></mods:language>';
+
+        $this->xpathGenerator->loop($xpath);
+
+        $this->assertEquals(
+            $expectedXml,
+            $this->xpathGenerator->getXML()
+        );
+    }
+
+}
