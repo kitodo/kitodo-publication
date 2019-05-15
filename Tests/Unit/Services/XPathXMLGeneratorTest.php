98594807082acd96653b8b1483718312e5c65cbd
Christopher Timm <timm@effective-webwork.de>
2019-05-15 09:00:10 +0200

    change test function names

--- Tests/Unit/Services/XPathXMLGeneratorTest.php
+++ Tests/Unit/Services/XPathXMLGeneratorTest.php
@@ -31,7 +31,7 @@ class XPathXMLGeneratorTest extends UnitTestCase
     /**
      * @test
      */
-    public function testXMLGeneration1()
+    public function testComplexElementsWithAttributes()
     {
         $xpath = 'mods:name[mods:role/mods:roleTerm[@type="code"][@authority="marcrelator"]="edt"]/mods:displayForm';
         $expectedXml = '<mods:name><mods:role><mods:roleTerm type="code" authority="marcrelator">edt</mods:roleTerm></mods:role><mods:displayForm/></mods:name>';
@@ -47,7 +47,7 @@ class XPathXMLGeneratorTest extends UnitTestCase
     /**
      * @test
      */
-    public function testXMLGeneration2()
+    public function testElementsWithMoreAttributes()
     {
         $xpath = 'mods:language/mods:languageTerm[@authority="iso639-2b"][@type="code"]="ger"';
         $expectedXml = '<mods:language><mods:languageTerm authority="iso639-2b" type="code">ger</mods:languageTerm></mods:language>';
@@ -63,7 +63,7 @@ class XPathXMLGeneratorTest extends UnitTestCase
     /**
      * @test
      */
-    public function testXMLGeneration3()
+    public function testSimpleElementWithAttribute()
     {
         $xpath = 'mods:originInfo[@eventType="publication"]';
         $expectedXml = '<mods:originInfo eventType="publication"/>';
@@ -79,10 +79,26 @@ class XPathXMLGeneratorTest extends UnitTestCase
     /**
      * @test
      */
-    public function testXMLGeneration4()
+    public function testValuesWithLinebreaks()
     {
         $xpath = "mods:originInfo[@eventType='publication']=\"A\nB\nC\n\"";
         $expectedXml = "<mods:originInfo eventType=\"publication\">A\nB\nC\n</mods:originInfo>";
+
+        $this->xpathGenerator->generateXmlFromXPath($xpath);
+
+        $this->assertEquals(
+            $expectedXml,
+            $this->xpathGenerator->getXML()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function testXMLGeneration5()
+    {
+        $xpath = "mods:originInfo[@xlink:href='test']";
+        $expectedXml = "<mods:originInfo xlink:href=\"test\"/>";
 
         $this->xpathGenerator->generateXmlFromXPath($xpath);
 
