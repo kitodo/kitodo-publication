df10566b3b83d3c704994d0fbd96f5fcafee7247
haogatyp <gatermann@effective-webwork.de>
2019-07-18 15:00:28 +0200

    Change extension dependency to typo3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version'          => '2.0.0',
     'constraints'      => array(
         'depends'   => array(
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '8.7.0-8.7.99',
         ),
         'conflicts' => array(
         ),
