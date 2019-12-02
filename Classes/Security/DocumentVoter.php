dea5cb1fc7b0b4630ac1537e2e6b0513b2623f87
haogatyp <gatermann@effective-webwork.de>
2019-12-02 13:30:49 +0100

    Fix: Researcher, show details for documents with the state IN_PROGRESS.

--- Classes/Security/DocumentVoter.php
+++ Classes/Security/DocumentVoter.php
@@ -289,6 +289,7 @@ class DocumentVoter extends Voter
                 $document->getOwner() === $this->security->getUser()->getUid() ||
                 $document->getState() === DocumentWorkflow::STATE_REGISTERED_NONE ||
                 $document->getState() === DocumentWorkflow::STATE_POSTPONED_NONE ||
+                $document->getState() === DocumentWorkflow::STATE_IN_PROGRESS_NONE ||
                 $document->getState() === DocumentWorkflow::STATE_IN_PROGRESS_ACTIVE ||
                 $document->getState() === DocumentWorkflow::STATE_IN_PROGRESS_DELETED ||
                 $document->getState() === DocumentWorkflow::STATE_IN_PROGRESS_INACTIVE
