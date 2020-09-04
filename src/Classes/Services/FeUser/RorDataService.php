4549cbe180b4b569357de6eba7dde96e6e7c34d7
Christopher Timm <timm@effective-webwork.de>
2020-09-04 08:33:55 +0200

    service api ror add urlencode

--- src/Classes/Services/FeUser/RorDataService.php
+++ src/Classes/Services/FeUser/RorDataService.php
@@ -12,8 +12,12 @@ class RorDataService
 
     }
 
+    public function searchTermReplacement($searchTerm) {
+        return urlencode($searchTerm);
+    }
+
     public function searchOrganisationRequest($searchTerm) {
-        $response = Request::get($this->apiUrl . '?query=' . $searchTerm)
+        $response = Request::get($this->apiUrl . '?query=' . $this->searchTermReplacement($searchTerm))
             ->send();
 
         return ['entries' => $response->body->items];
