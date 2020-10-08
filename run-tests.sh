d7374cbc668c61e23d06dfce53db59d4c8856009
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2020-10-08 13:42:19 +0200

    Fix docker build fail on clean project

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,5 @@ executable="vendor/phpunit/phpunit/phpunit"
 options="-c vendor/nimut/testing-framework/res/Configuration/UnitTests.xml $@"
 
 export ENV_HOST_IP=172.17.0.1
+docker network create qucosa_backend
 docker-compose -f Build/docker-compose.yml run --rm --no-deps ${container} ${executable} ${options} /app/Tests
