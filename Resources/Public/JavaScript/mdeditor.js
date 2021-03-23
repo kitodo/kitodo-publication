3e184e2831b4bf39f398c78008813f8d4db4b22e
Christopher Timm <timm@effective-webwork.de>
2021-03-23 16:52:42 +0100

    Add js library for markdown editor https://pandao.github.io/editor.md/en.html

---
+++ Resources/Public/JavaScript/mdeditor.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    loadMdEditor();
+    loadMdPreview();
+});
+
+var loadMdEditor = function () {
+    $("div[id^=markdown_area_]").each(function () {
+        var editor = editormd($(this).attr('id'), {
+            height : "500px",
+            path   : "typo3conf/ext/dpf/Resources/Public/JavaScript/editormd/lib/",
+            toolbarIcons : function() {
+                // Using "||" set icons align right.
+                return ["undo", "redo", "|", "bold", "del", "italic", "quote", "|", "h1", "h2", "h3", "h4", "h5", "h6", "|", "list-ul", "list-ol", "hr", "code", "preformatted-text", "code-block", "|", "help", "info", "||", "watch", "preview", "fullscreen"]
+            },
+        });
+    });
+}
+
+var loadMdPreview = function () {
+    $("div[id^=markdown_preview_]").each(function () {
+        var editor = editormd($(this).attr('id'), {
+            height: "500px",
+            readOnly: true,
+            path: "typo3conf/ext/dpf/Resources/Public/JavaScript/editormd/lib/",
+            toolbarIcons: function () {
+                // Using "||" set icons align right.
+                return []
+            },
+        });
+    });
+}
