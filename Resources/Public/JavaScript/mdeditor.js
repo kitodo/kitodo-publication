a6f1c3b2a695fd170cd21ef30b32daa4d3353607
Christopher Timm <timm@effective-webwork.de>
2021-03-25 17:09:38 +0100

    add readonly mode for markdown

--- Resources/Public/JavaScript/mdeditor.js
+++ Resources/Public/JavaScript/mdeditor.js
@@ -5,14 +5,20 @@ $(document).ready(function() {
 
 var loadMdEditor = function () {
     $("div[id^=markdown_area_]").each(function () {
-        var editor = editormd($(this).attr('id'), {
+        var editorSettings = {
             height : "500px",
             path   : "typo3conf/ext/dpf/Resources/Public/JavaScript/editormd/lib/",
             toolbarIcons : function() {
                 // Using "||" set icons align right.
                 return ["undo", "redo", "|", "bold", "del", "italic", "quote", "|", "h1", "h2", "h3", "h4", "h5", "h6", "|", "list-ul", "list-ol", "hr", "code", "preformatted-text", "code-block", "|", "help", "info", "||", "watch", "preview", "fullscreen"]
             },
-        });
+        };
+
+        if ($(this).find('textarea').attr('readonly') == 'readonly') {
+            editorSettings.readOnly = true;
+        }
+
+        var editor = editormd($(this).attr('id'), editorSettings);
     });
 }
 
