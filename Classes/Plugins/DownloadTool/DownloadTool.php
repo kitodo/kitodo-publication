b888ba97780a1d1ecfd23cb4e022cb5b2ed77721
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2021-04-07 12:06:24 +0200

    Make DownloadTool plugin compatible with Kitodo.Presentation 3.0

--- Classes/Plugins/DownloadTool/DownloadTool.php
+++ Classes/Plugins/DownloadTool/DownloadTool.php
@@ -24,8 +24,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * @subpackage    tx_dpf
  * @access    public
  */
-class DownloadTool extends \tx_dlf_plugin
+class DownloadTool extends \Kitodo\Dlf\Common\AbstractPlugin
 {
+    public $scriptRelPath = 'Classes/Plugins/DownloadTool.php';
 
     /**
      * The main method of the PlugIn
@@ -39,91 +40,56 @@ class DownloadTool extends \tx_dlf_plugin
      */
     public function main($content, $conf)
     {
-
         $this->init($conf);
 
         // get the tx_dpf.settings too
         // Flexform wins over TS
         $dpfTSconfig = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_dpf.'];
-
         if (is_array($dpfTSconfig['settings.'])) {
-
             \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($dpfTSconfig['settings.'], $this->conf, true, false);
             $this->conf = $dpfTSconfig['settings.'];
-
         }
 
         // Load current document.
         $this->loadDocument();
-
         if ($this->doc === null || empty($this->conf['fileGrpDownload'])) {
-
             // Quit without doing anything if required variables are not set.
             return $content;
-
         }
 
         // Load template file.
         if (!empty($this->conf['templateFile'])) {
-
-            $this->template = $this->cObj->getSubpart($this->cObj->fileResource($this->conf['templateFile']), '###TEMPLATE###');
-
+            $this->template = $this->templateService->getSubpart($this->cObj->fileResource($this->conf['templateFile']), '###TEMPLATE###');
         } else {
-
-            $this->template = $this->cObj->getSubpart($this->cObj->fileResource('EXT:dpf/Classes/Plugins/DownloadTool/template.tmpl'), '###TEMPLATE###');
-
+            $this->template = $this->templateService->getSubpart($this->cObj->fileResource('EXT:dpf/Classes/Plugins/DownloadTool/template.tmpl'), '###TEMPLATE###');
         }
 
-        $subpartArray['downloads'] = $this->cObj->getSubpart($this->template, '###DOWNLOADS###');
-
+        $subpartArray['downloads'] = $this->templateService->getSubpart($this->template, '###DOWNLOADS###');
         // Show all PDF documents in download filegroup
         $attachments = $this->getAttachments();
         // Get VG-Wort-Url
         $vgwort = $this->getVGWortUrl();
-
         $content = '';
-
         if (is_array($attachments)) {
-
             foreach ($attachments as $id => $file) {
-
                 $conf = array(
                     'useCacheHash'     => 0,
                     'parameter'        => $this->conf['apiPid'] . ' - piwik_download',
                     'additionalParams' => '&tx_dpf[qid]=' . $this->doc->recordId . '&tx_dpf[action]=attachment' . '&tx_dpf[attachment]=' . $file['ID'],
                     'forceAbsoluteUrl' => true,
                 );
-
                 $title = $file['LABEL'] ? $file['LABEL'] : $file['ID'];
+                $markerArray['###FILE###'] = $this->cObj->typoLink($title, $conf);
 
-                // Create a-tag without VG-Wort Redirect
-                if ($vgwort === FALSE) {
-
-                    // replace uid with URI to dpf API
-                    $markerArray['###FILE###'] = $this->cObj->typoLink($title, $conf);
-
-                    // Create a-tag with VG-Wort Redirect
-                } elseif(!empty($vgwort)) {
-
-                    $qucosaUrl = urlencode($this->cObj->typoLink_URL($conf));
-
-                    $confVgwort = array(
-                        'useCacheHash'     => 0,
-                        'parameter'        => $vgwort . $qucosaUrl . ' - piwik_download',
-                    );
-
-                    $markerArray['###FILE###'] = $this->cObj->typoLink($title, $confVgwort);
-
+                if(!empty($vgwort)) {
+                    $markerArray['###VGWORT###'] = "<div class='div_vgwpixel' data-url='" . $vgwort . "'></div>";
+                } else {
+                    $markerArray['###VGWORT###'] = "";
                 }
-
                 $content .= $this->cObj->substituteMarkerArray($subpartArray['downloads'], $markerArray);
-
             }
-
         }
-
         return $this->cObj->substituteSubpart($this->template, '###DOWNLOADS###', $content, true);
-
     }
 
     /**
@@ -133,72 +99,43 @@ class DownloadTool extends \tx_dlf_plugin
      */
     protected function getAttachments()
     {
-
         // Get pdf documents
         $xPath = 'mets:fileSec/mets:fileGrp[@USE="' . $this->conf['fileGrpDownload'] . '"]/mets:file';
-
         $files = $this->doc->mets->xpath($xPath);
-
         if (!is_array($files)) {
-
             return array();
-
         }
-
         foreach ($files as $key => $file) {
-
             $singleFile = array();
-
             foreach ($file->attributes('mext', 1) as $attribute => $value) {
-
                 $singleFile[$attribute] = $value;
-
             }
-
             foreach ($file->attributes() as $attribute => $value) {
-
                 $singleFile[$attribute] = $value;
-
             }
-
             $attachments[(string) $singleFile['ID']] = $singleFile;
-
         }
-
         if (is_array($attachments) && count($attachments) > 1) {
-
             ksort($attachments);
-
         }
-
         return $attachments;
     }
 
     protected function getVGWortUrl()
     {
-
         // Get VG-Wort-OpenKey for document
         $this->doc->mets->registerXPathNamespace("slub", 'http://slub-dresden.de/');
         $xPath = '//slub:info/slub:vgwortOpenKey';
         $vgwortOpenKey = $this->doc->mets->xpath($xPath)[0];
 
         if (!empty($vgwortOpenKey) or $vgwortOpenKey != FALSE ) {
-
             if (GeneralUtility::getIndpEnv('TYPO3_SSL')) {
-
                 $vgwortserver = 'https://ssl-vg03.met.vgwort.de/na/';
-
             } else {
-
                 $vgwortserver = 'http://vg08.met.vgwort.de/na/';
-
             }
-
-            return $vgworturl = $vgwortserver . $vgwortOpenKey . '?l=';
-
+            return $vgworturl = $vgwortserver . $vgwortOpenKey;
         }
-
         return FALSE;
     }
-
 }
