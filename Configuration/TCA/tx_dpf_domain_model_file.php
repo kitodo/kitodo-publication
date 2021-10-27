23047f6becd16ec22852f72c2ea9b35a900008a8
Mathias Korepkat <mathias.korepkat@slub-dresden.de>
2021-10-27 07:58:22 +0200

    Fix error when copying files
    Added missing parent language default value.

--- Configuration/TCA/tx_dpf_domain_model_file.php
+++ Configuration/TCA/tx_dpf_domain_model_file.php
@@ -72,6 +72,7 @@ return array(
                 'items'               => array(
                     array('', 0),
                 ),
+                'default' => 0,
                 'foreign_table'       => 'tx_dpf_domain_model_file',
                 'foreign_table_where' => 'AND tx_dpf_domain_model_file.pid=###CURRENT_PID### AND tx_dpf_domain_model_file.sys_language_uid IN (-1,0)',
             ),
