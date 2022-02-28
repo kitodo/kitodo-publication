466757641ba695dfbff595f9601ab552f8695819
haogatyp <gatermann@effective-webwork.de>
2022-02-28 11:46:24 +0100

    Fix colon issue in uri while updating resource tupel.

--- Classes/Services/Storage/Fedora/FedoraTransaction.php
+++ Classes/Services/Storage/Fedora/FedoraTransaction.php
@@ -543,13 +543,13 @@ class FedoraTransaction
         $errorMessage = 'Error while updating resource tuple "'. $requestUri .'". ';
 
         try {
-            $client = new Client(['base_uri' => $this->baseUri()]);
+            $client = new Client(['base_uri' => $requestUri]);
 
             $changedValues = $resourceTuple->getModifiedValues();
 
             // To avoid multiple entries of the same field we need to delete all existing entries of each changed field.
             foreach ($changedValues as $key => $value) {
-                 $response = $client->request('PATCH', $path,
+                 $response = $client->request('PATCH', '',
                     [
                         'auth' => [
                             $this->clientConfigurationManager->getFedoraUser(),
@@ -581,7 +581,7 @@ class FedoraTransaction
                 $insert[] = '<> <' . $value['uri'] . '> "' . $value['value'] . '"';
             }
 
-            $insertResponse = $client->request('PATCH', $path,
+            $insertResponse = $client->request('PATCH', $requestUri,
                 [
                     'auth' => [
                         $this->clientConfigurationManager->getFedoraUser(),
