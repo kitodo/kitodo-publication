a4277a1fed0850f69ea70bc3fdaa22e582b8f976
Erik Sommer <Erik.Sommer@slub-dresden.de>
2025-01-26 14:57:01 +0100

    Add phpstan dev dependency and configuration
    Signed-off-by: Erik Sommer <Erik.Sommer@slub-dresden.de>

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+// Custom bootstrap file for TYPO3 PHPStan analysis
+if (!defined('TYPO3_MODE')) {
+    define('TYPO3_MODE', 'BE'); // Backend mode
+}
+
+// Load TYPO3's autoload file
+require_once __DIR__ . '/vendor/autoload.php';
