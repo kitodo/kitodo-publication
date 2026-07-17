8b592eb71c9eb9efdff97e6be12459ca4d7590c7
Erik Sommer <erik.sommer@slub-dresden.de>
2026-07-17 12:14:44 +0200

    #2039: register dpfFixMetadataXpathAndLabels upgrade wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -290,6 +290,9 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dpfMigrateDl
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dpfFixIsbnMetadataCollision'] =
     \EWW\Dpf\Updates\FixIsbnMetadataCollisionUpdate::class;
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dpfFixMetadataXpathAndLabels'] =
+    \EWW\Dpf\Updates\FixMetadataXpathAndLabelsUpdate::class;
+
 // Public search results vary per request and are never page-cached (plugin runs as USER_INT),
 // so its GET parameters carry no caching risk and don't need a cHash.
 // CacheHashCalculator::isExcludedParameter() does an exact match per key, no wildcards.
