f25e284e22a00260b9b52cfe6e5f8ae2393eee48
Erik Sommer <erik.sommer@slub-dresden.de>
2026-09-07 17:33:26 +0200

    #2040: add missing CSS class hook to AutorIn (Institution) row
    corporation_author (uid 547) had no CSS class hook, while its sibling
    corporation_editor (uid 43) has had class="editor" since bde2f342.
    Both institution roles need the same styling - role label small/grey,
    name large/black - but the CSS had nothing to select the AutorIn row
    by.
    
    Adds class="author" to the row's <dt>/<dd> wrap, matching the class
    the personal-author row already uses.

--- ext_localconf.php
+++ ext_localconf.php
@@ -392,6 +392,9 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dpfAddRelati
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dpfHideDuplicateSequenceRows'] =
     \EWW\Dpf\Updates\HideDuplicateSequenceRowsUpdate::class;
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['dpfFixInstitutionAuthorClassHook'] =
+    \EWW\Dpf\Updates\FixInstitutionAuthorClassHookUpdate::class;
+
 // Public search results vary per request and are never page-cached (plugin runs as USER_INT),
 // so its GET parameters carry no caching risk and don't need a cHash.
 // CacheHashCalculator::isExcludedParameter() does an exact match per key, no wildcards.
